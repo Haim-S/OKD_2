40b291aa9a3bb9aa354254f91c72faae69577235
HaimSlbah <26life72@gmail.com>
2023-05-10 13:52:58 +0300

    sidebar fix

--- src/components/navbar/SideBar.js
+++ src/components/navbar/SideBar.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import Link from 'next/link'
 import {AiOutlineClose} from "react-icons/ai"
+import { PAGES_PATH } from '@/constant/PATH'
+import Icons from '../common/Icons'
 
 const SideBar = ({nav, handleNav}) => {
   return (
@@ -9,7 +12,7 @@ const SideBar = ({nav, handleNav}) => {
  }
 >
   <div className={nav? 
-  'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500': 
+  'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ffff] p-10 ease-in duration-500': 
    'fixed left-[-100%] top-0 p-10 ease-in duration-500'}
   >
     <div className='flex w-full items-center justify-between'>
@@ -18,7 +21,28 @@ const SideBar = ({nav, handleNav}) => {
         <AiOutlineClose />
       </div>
     </div>
+    <div className='border-b border-gray-300 my-4'>
+        <p className='w-[85%] md:w-[90%] py-4'>Come build something legendary together</p>
+    </div>
+
+    <div className='py-4 flex flex-col'>
+        <ul className='uppercase'>
+        {PAGES_PATH.map((p, i)=>{
+                return(
+                  <Link key={i} href={p.path}>
+                   <li onClick={handleNav} className='py-4 text-sm'>{p.linkLabel}</li>
+                </Link>
+                )
+              })}
+        </ul>
+        <div className='pt-28'>
+              <p className='uppercase tracking-widest text-[#4A4A4A]'>Let's Connect</p>
+              <Icons nav={nav}/>  
+        </div>
+    </div>
+
   </div>
+
 </div>
   )
 }
