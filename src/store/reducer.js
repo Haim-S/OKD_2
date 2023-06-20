4c351f7287c7270647cbec00f1023523772c16f5
HaimSlbah <26life72@gmail.com>
2023-06-20 19:35:47 +0300

    store for accessibilty

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import translationsReducer from "./Slices/translationsSlice";
+import accessibilityReducer from "./Slices/accessibilitySlice";
 
 export const reducer = combineReducers({
-    translations: translationsReducer
+    translations: translationsReducer,
+    accessibility: accessibilityReducer
 });
